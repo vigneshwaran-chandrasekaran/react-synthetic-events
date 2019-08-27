a60b591a15eaf130847907e5ac0011b496d2aff4
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-08-27 12:23:10 +0530

    Updates

---
+++ precache-manifest.64f2a6132209af049164a0b6442ae8b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beef79bd3be30610f46d847843b2049d",
+    "url": "/react-synthetic-events/index.html"
+  },
+  {
+    "revision": "79773b03e55b506ed5cf",
+    "url": "/react-synthetic-events/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "41ae73a5d298896a015f",
+    "url": "/react-synthetic-events/static/js/2.bcdd2817.chunk.js"
+  },
+  {
+    "revision": "79773b03e55b506ed5cf",
+    "url": "/react-synthetic-events/static/js/main.805e530f.chunk.js"
+  },
+  {
+    "revision": "383c3e64de1ec0706bb3",
+    "url": "/react-synthetic-events/static/js/runtime~main.e8f691ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-synthetic-events/static/media/logo.5d5d9eef.svg"
+  }
+]);
