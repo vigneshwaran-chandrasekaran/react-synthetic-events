3b36678e442c9cf03f112d5ff50144c88fada31c
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-08-30 12:36:05 +0530

    added sample image and explanation text for pointer events

--- src/components/OnPointer.js
+++ src/components/OnPointer.js
@@ -5,6 +5,7 @@ export default function OnPointer() {
     const [state, setstate] = useState({});
 
     const handleAction = (e) => {
+        console.log(typeof e);
         console.log({ e });
         e.persist();
         setstate(e);
@@ -15,20 +16,24 @@ export default function OnPointer() {
     }
     return (
         <div>
-            <p className="bg-warning p-2"><strong>onPointer</strong> Event</p>
-            <p onPointerDown={handleAction}>
-                Lorem Ipsum is simply dummy text of
-                the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic typesetting,
-                remaining essentially unchanged. It was popularised in the 1960s with
-                the release of Letraset sheets containing Lorem Ipsum passages,
-                and more recently with desktop publishing software like Aldus
-                PageMaker including versions of Lorem Ipsum.
+            <p className="bg-warning p-2 mt-5"><strong>onPointer</strong> Event</p>
+            <div className='text-center'>
+                <img src='https://www.w3.org/TR/pointerevents/pointer.png' alt='pointer-event' />
+            </div>
+            <p className='my-3 text-success'>
+                Click/touch below text to trigger pointer events
             </p>
-            <table>
+            <p onPointerDown={handleAction}>
+                The Pointer Events API is an HTML5 specification that combines touch, mouse, pen and other inputs into a single unified API.
+                <br />
+                The simple idea behind it, is that mouse and touch and stylus input events are unified into a single event API.
+            </p>
+            <div>
+                <a href='https://mobiforge.com/design-development/html5-pointer-events-api-combining-touch-mouse-and-pen' target='_blank'>
+                    for more details about pointer events click here
+                </a>
+            </div>
+            <table className='table my-3'>
                 <tbody>
                     <tr>
                         <th>Property</th>
