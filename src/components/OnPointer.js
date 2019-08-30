49096fb1ed74cc28b40f9677646421f48b1d3d2c
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-08-30 13:12:11 +0530

    all pointer events are added

--- src/components/OnPointer.js
+++ src/components/OnPointer.js
@@ -11,20 +11,44 @@ export default function OnPointer() {
     };
     return (
         <div>
-            <p className="bg-warning p-2 mt-5"><strong>onPointer</strong> Event</p>
+            <p className="bg-success p-2 mt-5"><strong>onPointer</strong> Event</p>
             <div className='text-center'>
                 <img src='https://www.w3.org/TR/pointerevents/pointer.png' alt='pointer-event' />
             </div>
             <p className='my-3 text-success'>
                 Click/touch below text to trigger pointer events
             </p>
-            <p onPointerDown={handleAction}>
-                The Pointer Events API is an HTML5 specification that combines touch, mouse, pen and other inputs into a single unified API.
-                <br />
-                The simple idea behind it, is that mouse and touch and stylus input events are unified into a single event API.
-            </p>
+            <div
+                onPointerDown={handleAction}
+                onPointerMove={handleAction}
+                onPointerUp={handleAction}
+                onPointerCancel={handleAction}
+                onGotPointerCapture={handleAction}
+                onLostPointerCapture={handleAction}
+                onPointerEnter={handleAction}
+                onPointerLeave={handleAction}
+                onPointerOver={handleAction}
+                onPointerOut={handleAction}
+            >
+                <p>
+                    The Pointer Events API is an HTML5 specification that combines touch, mouse, pen and other inputs into a single unified API.
+                </p>
+                <p>
+                    The simple idea behind it, is that mouse and touch and stylus input events are unified into a single event API.
+                </p>
+                <p className='font-weight-bold'>
+                    List of available pointer events
+                </p>
+                <p>
+                    onPointerDownonPointerMove, onPointerUp, onPointerCancel, onGotPointerCapture,
+                    onLostPointerCapture, onPointerEnter, onPointerLeave, onPointerOver, onPointerOut
+                </p>
+            </div>
             <div>
-                <a href='https://mobiforge.com/design-development/html5-pointer-events-api-combining-touch-mouse-and-pen' target='_blank'>
+                <a href='https://mobiforge.com/design-development/html5-pointer-events-api-combining-touch-mouse-and-pen'
+                    target='_blank'
+                    rel="noopener noreferrer"
+                >
                     for more details about pointer events click here
                 </a>
             </div>
