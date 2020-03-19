80f425683bb3e95e318bb1b6831e0dd39d609626
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-03-19 20:14:35 +0530

    common import index.js file added in components folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 // import { Button } from 'react-bootstrap';
 // import Navbar from './components/Navbar';
-import OnCopy from './components/OnCopy';
-import OnCut from './components/OnCut';
-import OnPaste from './components/OnPaste';
-import AllClipboard from './components/AllClipboard';
-import OnPointer from './components/OnPointer';
-import OnMouse from './components/OnMouse';
-import OnKeyboard from './components/OnKeyboard';
-import OnFocus from './components/OnFocus';
-import OnComposition from './components/OnComposition';
-import OnSelect from './components/OnSelect';
-import UiEvents from './components/UiEvents';
-import WheelEvents from './components/WheelEvents';
-import ImageEvents from './components/ImageEvents';
+import OnCopy from "./components/OnCopy";
+import OnCut from "./components/OnCut";
+import OnPaste from "./components/OnPaste";
+import OnPointer from "./components/OnPointer";
+import OnMouse from "./components/OnMouse";
+import OnKeyboard from "./components/OnKeyboard";
+import OnFocus from "./components/OnFocus";
+import OnComposition from "./components/OnComposition";
+import OnSelect from "./components/OnSelect";
+import UiEvents from "./components/UiEvents";
+import WheelEvents from "./components/WheelEvents";
+import ImageEvents from "./components/ImageEvents";
 
+import { AllClipboard } from "./components";
 
 function App() {
   return (
